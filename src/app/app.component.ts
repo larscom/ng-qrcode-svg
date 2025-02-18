17bbccb46314ce0db2ff662ef0d20ddbb2646d4f
larscom <larskniep@gmail.com>
2025-02-18 16:02:20 +0100

    update ng 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { FormBuilder } from '@angular/forms';
 import { map, shareReplay, startWith } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   private readonly initialValue = {
