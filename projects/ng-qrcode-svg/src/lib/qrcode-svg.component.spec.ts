7cf45b3fce9652183c4a027baf9c8d1c98da9365
larscom <larskniep@gmail.com>
2025-11-14 12:18:43 +0100

    create signal based inputs

--- projects/ng-qrcode-svg/src/lib/qrcode-svg.component.spec.ts
+++ projects/ng-qrcode-svg/src/lib/qrcode-svg.component.spec.ts
@@ -1,8 +1,6 @@
-import { SimpleChanges } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { By } from '@angular/platform-browser'
 import { Shallow } from 'shallow-render'
-
 import { QrcodeSvgComponent } from './qrcode-svg.component'
 
 describe('QrcodeSvgComponent', () => {
@@ -59,158 +57,21 @@ describe('QrcodeSvgComponent', () => {
 
       fixture = TestBed.createComponent(QrcodeSvgComponent)
       component = fixture.componentInstance
-      fixture.detectChanges()
     })
 
     it(`should create viewBox based on borderSize`, () => {
-      component.value = 'test'
-      component.borderSize = 4
+      fixture.componentRef.setInput('value', 'test')
+      fixture.componentRef.setInput('borderSize', 4)
 
-      component.ngOnChanges({})
-
-      expect(component.viewBox).toEqual('0 0 29 29')
+      expect(component.viewBox()).toEqual('0 0 29 29')
     })
 
     it(`should create d attribute based on borderSize`, () => {
-      component.value = 'test'
-      component.borderSize = 4
+      fixture.componentRef.setInput('value', 'test')
+      fixture.componentRef.setInput('borderSize', 4)
 
-      component.ngOnChanges({})
-
-      expect(component.d).toEqual(
+      expect(component.d()).toEqual(
         'M4,4h1v1h-1z M5,4h1v1h-1z M6,4h1v1h-1z M7,4h1v1h-1z M8,4h1v1h-1z M9,4h1v1h-1z M10,4h1v1h-1z M12,4h1v1h-1z M13,4h1v1h-1z M14,4h1v1h-1z M16,4h1v1h-1z M18,4h1v1h-1z M19,4h1v1h-1z M20,4h1v1h-1z M21,4h1v1h-1z M22,4h1v1h-1z M23,4h1v1h-1z M24,4h1v1h-1z M4,5h1v1h-1z M10,5h1v1h-1z M13,5h1v1h-1z M14,5h1v1h-1z M18,5h1v1h-1z M24,5h1v1h-1z M4,6h1v1h-1z M6,6h1v1h-1z M7,6h1v1h-1z M8,6h1v1h-1z M10,6h1v1h-1z M14,6h1v1h-1z M18,6h1v1h-1z M20,6h1v1h-1z M21,6h1v1h-1z M22,6h1v1h-1z M24,6h1v1h-1z M4,7h1v1h-1z M6,7h1v1h-1z M7,7h1v1h-1z M8,7h1v1h-1z M10,7h1v1h-1z M12,7h1v1h-1z M15,7h1v1h-1z M16,7h1v1h-1z M18,7h1v1h-1z M20,7h1v1h-1z M21,7h1v1h-1z M22,7h1v1h-1z M24,7h1v1h-1z M4,8h1v1h-1z M6,8h1v1h-1z M7,8h1v1h-1z M8,8h1v1h-1z M10,8h1v1h-1z M14,8h1v1h-1z M15,8h1v1h-1z M18,8h1v1h-1z M20,8h1v1h-1z M21,8h1v1h-1z M22,8h1v1h-1z M24,8h1v1h-1z M4,9h1v1h-1z M10,9h1v1h-1z M13,9h1v1h-1z M15,9h1v1h-1z M18,9h1v1h-1z M24,9h1v1h-1z M4,10h1v1h-1z M5,10h1v1h-1z M6,10h1v1h-1z M7,10h1v1h-1z M8,10h1v1h-1z M9,10h1v1h-1z M10,10h1v1h-1z M12,10h1v1h-1z M14,10h1v1h-1z M16,10h1v1h-1z M18,10h1v1h-1z M19,10h1v1h-1z M20,10h1v1h-1z M21,10h1v1h-1z M22,10h1v1h-1z M23,10h1v1h-1z M24,10h1v1h-1z M13,11h1v1h-1z M16,11h1v1h-1z M6,12h1v1h-1z M8,12h1v1h-1z M9,12h1v1h-1z M10,12h1v1h-1z M12,12h1v1h-1z M13,12h1v1h-1z M17,12h1v1h-1z M21,12h1v1h-1z M24,12h1v1h-1z M7,13h1v1h-1z M8,13h1v1h-1z M11,13h1v1h-1z M12,13h1v1h-1z M16,13h1v1h-1z M18,13h1v1h-1z M19,13h1v1h-1z M23,13h1v1h-1z M24,13h1v1h-1z M4,14h1v1h-1z M5,14h1v1h-1z M6,14h1v1h-1z M7,14h1v1h-1z M8,14h1v1h-1z M10,14h1v1h-1z M12,14h1v1h-1z M16,14h1v1h-1z M17,14h1v1h-1z M18,14h1v1h-1z M19,14h1v1h-1z M21,14h1v1h-1z M22,14h1v1h-1z M23,14h1v1h-1z M24,14h1v1h-1z M4,15h1v1h-1z M5,15h1v1h-1z M6,15h1v1h-1z M7,15h1v1h-1z M9,15h1v1h-1z M12,15h1v1h-1z M13,15h1v1h-1z M15,15h1v1h-1z M16,15h1v1h-1z M19,15h1v1h-1z M20,15h1v1h-1z M23,15h1v1h-1z M4,16h1v1h-1z M5,16h1v1h-1z M9,16h1v1h-1z M10,16h1v1h-1z M12,16h1v1h-1z M13,16h1v1h-1z M14,16h1v1h-1z M19,16h1v1h-1z M21,16h1v1h-1z M23,16h1v1h-1z M24,16h1v1h-1z M12,17h1v1h-1z M14,17h1v1h-1z M15,17h1v1h-1z M16,17h1v1h-1z M21,17h1v1h-1z M24,17h1v1h-1z M4,18h1v1h-1z M5,18h1v1h-1z M6,18h1v1h-1z M7,18h1v1h-1z M8,18h1v1h-1z M9,18h1v1h-1z M10,18h1v1h-1z M13,18h1v1h-1z M14,18h1v1h-1z M20,18h1v1h-1z M21,18h1v1h-1z M23,18h1v1h-1z M24,18h1v1h-1z M4,19h1v1h-1z M10,19h1v1h-1z M12,19h1v1h-1z M13,19h1v1h-1z M14,19h1v1h-1z M16,19h1v1h-1z M18,19h1v1h-1z M23,19h1v1h-1z M4,20h1v1h-1z M6,20h1v1h-1z M7,20h1v1h-1z M8,20h1v1h-1z M10,20h1v1h-1z M12,20h1v1h-1z M14,20h1v1h-1z M15,20h1v1h-1z M18,20h1v1h-1z M20,20h1v1h-1z M21,20h1v1h-1z M23,20h1v1h-1z M24,20h1v1h-1z M4,21h1v1h-1z M6,21h1v1h-1z M7,21h1v1h-1z M8,21h1v1h-1z M10,21h1v1h-1z M13,21h1v1h-1z M18,21h1v1h-1z M19,21h1v1h-1z M23,21h1v1h-1z M4,22h1v1h-1z M6,22h1v1h-1z M7,22h1v1h-1z M8,22h1v1h-1z M10,22h1v1h-1z M12,22h1v1h-1z M16,22h1v1h-1z M17,22h1v1h-1z M19,22h1v1h-1z M22,22h1v1h-1z M24,22h1v1h-1z M4,23h1v1h-1z M10,23h1v1h-1z M13,23h1v1h-1z M14,23h1v1h-1z M15,23h1v1h-1z M17,23h1v1h-1z M18,23h1v1h-1z M19,23h1v1h-1z M20,23h1v1h-1z M21,23h1v1h-1z M23,23h1v1h-1z M4,24h1v1h-1z M5,24h1v1h-1z M6,24h1v1h-1z M7,24h1v1h-1z M8,24h1v1h-1z M9,24h1v1h-1z M10,24h1v1h-1z M14,24h1v1h-1z M17,24h1v1h-1z M18,24h1v1h-1z M19,24h1v1h-1z M22,24h1v1h-1z M23,24h1v1h-1z M24,24h1v1h-1z'
-      )
-    })
-
-    it('should skip update if backgroundColor changed', () => {
-      component.value = 'value'
-
-      const firstChange = false
-      const changes: SimpleChanges = {
-        backgroundColor: {
-          firstChange,
-          isFirstChange: () => firstChange,
-          currentValue: '#FF0000',
-          previousValue: component.backgroundColor
-        }
-      }
-
-      component.ngOnChanges(changes)
-
-      expect(component.d).toBeUndefined()
-    })
-
-    it('should NOT skip update if backgroundColor changed for first time', () => {
-      component.value = 'value'
-
-      const firstChange = true
-      const changes: SimpleChanges = {
-        backgroundColor: {
-          firstChange,
-          isFirstChange: () => firstChange,
-          currentValue: '#FF0000',
-          previousValue: component.backgroundColor
-        }
-      }
-
-      component.ngOnChanges(changes)
-
-      expect(component.d).toBeDefined()
-    })
-
-    it('should skip update if foregroundColor changed', () => {
-      component.value = 'value'
-
-      const firstChange = false
-      const changes: SimpleChanges = {
-        foregroundColor: {
-          firstChange,
-          isFirstChange: () => firstChange,
-          currentValue: '#FF0000',
-          previousValue: component.foregroundColor
-        }
-      }
-
-      component.ngOnChanges(changes)
-
-      expect(component.d).toBeUndefined()
-    })
-
-    it('should NOT skip update if foregroundColor changed for first time', () => {
-      component.value = 'value'
-
-      const firstChange = true
-      const changes: SimpleChanges = {
-        backgroundColor: {
-          firstChange,
-          isFirstChange: () => firstChange,
-          currentValue: '#FF0000',
-          previousValue: component.foregroundColor
-        }
-      }
-
-      component.ngOnChanges(changes)
-
-      expect(component.d).toBeDefined()
-    })
-
-    it('should skip update if size changed', () => {
-      component.value = 'value'
-
-      const firstChange = false
-      const changes: SimpleChanges = {
-        size: {
-          firstChange,
-          isFirstChange: () => firstChange,
-          currentValue: 300,
-          previousValue: component.size
-        }
-      }
-
-      component.ngOnChanges(changes)
-
-      expect(component.d).toBeUndefined()
-    })
-
-    it('should NOT skip update if size changed for first time', () => {
-      component.value = 'value'
-
-      const firstChange = true
-      const changes: SimpleChanges = {
-        size: {
-          firstChange,
-          isFirstChange: () => firstChange,
-          currentValue: 300,
-          previousValue: component.foregroundColor
-        }
-      }
-
-      component.ngOnChanges(changes)
-
-      expect(component.d).toBeDefined()
-    })
-
-    it('should throw error if value is missing', () => {
-      component.value = ''
-
-      expect(() => component.ngOnChanges({})).toThrow(Error('[@larscom/ng-qrcode-svg] You must provide a value!'))
-    })
-
-    it(`should throw error if foregroundColor doesn't match hex pattern`, () => {
-      component.value = 'value'
-      component.foregroundColor = 'invalid'
-
-      expect(() => component.ngOnChanges({})).toThrow(
-        Error('[@larscom/ng-qrcode-svg] You must provide a valid foregroundColor (HEX RGB) eg: #000000')
-      )
-    })
-
-    it(`should throw error if backgroundColor doesn't match hex pattern`, () => {
-      component.value = 'value'
-      component.backgroundColor = 'invalid'
-
-      expect(() => component.ngOnChanges({})).toThrow(
-        Error('[@larscom/ng-qrcode-svg] You must provide a valid backgroundColor (HEX RGB) eg: #FFFFFF')
       )
     })
   })
--- projects/ng-qrcode-svg/src/lib/qrcode-svg.component.ts
+++ projects/ng-qrcode-svg/src/lib/qrcode-svg.component.ts
@@ -1,7 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core'
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
 import { Ecc, QrCode } from './qrcode-generator'
-
-const VALID_COLOR_REGEX = /^#(?:[0-9a-fA-F]{3,4}){1,2}$/
 
 @Component({
   selector: 'qrcode-svg',
@@ -11,69 +9,47 @@ const VALID_COLOR_REGEX = /^#(?:[0-9a-fA-F]{3,4}){1,2}$/
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
       stroke="none"
-      [attr.alt]="alt"
-      [attr.aria-label]="ariaLabel"
-      [attr.width]="size"
-      [attr.height]="size"
-      [attr.viewBox]="viewBox"
+      [attr.alt]="alt()"
+      [attr.aria-label]="ariaLabel()"
+      [attr.width]="size()"
+      [attr.height]="size()"
+      [attr.viewBox]="viewBox()"
     >
-      <rect width="100%" height="100%" [attr.fill]="backgroundColor" />
-      <path [attr.d]="d" [attr.fill]="foregroundColor" />
+      <rect width="100%" height="100%" [attr.fill]="backgroundColor()" />
+      <path [attr.d]="d()" [attr.fill]="foregroundColor()" />
     </svg>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class QrcodeSvgComponent implements OnChanges {
-  @Input() value!: string
-  @Input() ecl: 'low' | 'medium' | 'quartile' | 'high' = 'medium'
-  @Input() borderSize = 2
+export class QrcodeSvgComponent {
+  value = input.required<string>()
+  ecl = input<'low' | 'medium' | 'quartile' | 'high'>('medium')
+  borderSize = input<number>(2)
+  size = input<string | number>(250)
 
-  @Input() size: string | number = 250
-  @Input() backgroundColor = '#FFFFFF'
-  @Input() foregroundColor = '#000000'
+  backgroundColor = input<string>('#FFFFFF')
+  foregroundColor = input<string>('#000000')
 
-  @Input() alt: string | undefined
-  @Input() ariaLabel: string | undefined
+  alt = input<string>()
+  ariaLabel = input<string>()
 
-  private qr!: QrCode
+  viewBox = computed(() => {
+    const s = this.qr().size + this.borderSize() * 2
+    return `0 0 ${s} ${s}`
+  })
+  d = computed(() => {
+    return this.createD(this.qr(), this.borderSize())
+  })
 
-  viewBox!: string
-  d!: string
+  private qr = computed(() => {
+    return QrCode.encodeText(this.value(), Ecc[this.ecl()])
+  })
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.validateInputs()
-
-    if (this.skipUpdate(changes)) return
-
-    this.qr = QrCode.encodeText(this.value, Ecc[this.ecl])
-    const s = this.qr.size + this.borderSize * 2
-    this.viewBox = `0 0 ${s} ${s}`
-    this.d = this.createD(this.borderSize)
-  }
-
-  private validateInputs(): void {
-    if (!this.value) throw Error('[@larscom/ng-qrcode-svg] You must provide a value!')
-
-    if (!VALID_COLOR_REGEX.test(this.backgroundColor))
-      throw Error('[@larscom/ng-qrcode-svg] You must provide a valid backgroundColor (HEX RGB) eg: #FFFFFF')
-
-    if (!VALID_COLOR_REGEX.test(this.foregroundColor))
-      throw Error('[@larscom/ng-qrcode-svg] You must provide a valid foregroundColor (HEX RGB) eg: #000000')
-  }
-
-  private skipUpdate({ backgroundColor, foregroundColor, size }: SimpleChanges): boolean {
-    const bgColorChanged = backgroundColor?.currentValue && !backgroundColor?.firstChange
-    const fgColorChanged = foregroundColor?.currentValue && !foregroundColor.firstChange
-    const sizeChanged = size?.currentValue && !size.firstChange
-
-    return bgColorChanged || fgColorChanged || sizeChanged
-  }
-
-  private createD(borderSize: number): string {
+  private createD(qr: QrCode, borderSize: number): string {
     const parts: string[] = []
-    for (let y = 0; y < this.qr.size; y++) {
-      for (let x = 0; x < this.qr.size; x++) {
-        if (this.qr.getModule(x, y)) parts.push(`M${x + borderSize},${y + borderSize}h1v1h-1z`)
+    for (let y = 0; y < qr.size; y++) {
+      for (let x = 0; x < qr.size; x++) {
+        if (qr.getModule(x, y)) parts.push(`M${x + borderSize},${y + borderSize}h1v1h-1z`)
       }
     }
     return parts.join(' ')
